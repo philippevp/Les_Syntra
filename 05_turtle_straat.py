78ddf7f6acb8c62d2007e8fa4435a7cb9e57fcc2
Jef <jefvanrompay@gmail.com>
2022-10-04 21:17:40 +0200

    lists

--- 05_turtle_straat.py
+++ 05_turtle_straat.py
@@ -1,2 +1,40 @@
 import turtle as turtle
-from utils.usefull_functions import
+from utils.usefull_functions import teken_huis
+
+
+coordinaten = []
+
+coordinaten.append((0,0))
+
+# coordinaten.append("a")
+# coordinaten.append("b")
+# coordinaten.append("a")
+
+print(coordinaten)
+# coordinaten.append("a", "b")
+
+coordinaten.append((100, 100))
+
+print(coordinaten)
+# laatste = coordinaten.pop()
+# print(laatste)
+print(coordinaten)
+
+
+print(type("a"))
+print(type((0,0)))
+print(type(5))
+
+print(coordinaten[0])
+
+for x in range(len(coordinaten)):
+    print(coordinaten[x])
+
+for coor in coordinaten:
+    print(coor)
+
+
+
+# for data in list:
+#     try:
+#         int(data)
