497cfa669de13397eec3052fecc1d0949fb931c8
Jef <jefvanrompay@gmail.com>
2022-10-06 21:57:56 +0200

    class Point

---
+++ utils/usefulll_classes.py
@@ -0,0 +1,17 @@
+
+
+class Point:
+    '''
+    Dit is een punt in een 2D ruimte
+    '''
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    # geeft info over het object
+    def __repr__(self):
+        return f"Point ({self.x}, {self.y})"
+
+    # gebruikt door de print functie -- user friendly
+    def __str__(self):
+        return f"({self.x}, {self.y})"    
