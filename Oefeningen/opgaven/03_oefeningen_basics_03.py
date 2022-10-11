cb8fdb7fd80f95cc1bb271a92532f9e5981abb1f
Jef <jefvanrompay@gmail.com>
2022-10-11 21:07:33 +0200

    oefeningen_3

---
+++ Oefeningen/opgaven/03_oefeningen_basics_03.py
@@ -0,0 +1,110 @@
+'''
+02_ nodig (met date time voor laatste oef)
+'''
+
+# 1. 
+# De gebruiker typt de leeftijd van elke bezoeker van een pretpark.
+# De gebruiker typt op het einde van de dag 0 als leeftijd.
+# Toon het aantal bezoekers dat korting kreeg.
+# Een bezoeker krijgt korting als zijn leeftijd kleiner is dan 7 of groter dan 80
+
+# 2. 
+# De gebruiker typt temperaturen tot hij 777 typt.
+# Toon daarna enkel de vriestemperaturen (temperaturen <= 0).
+
+# 3.
+#  De gebruiker tikt de naam, de lengte en het gewicht van personen tot hij stop als naam typt.
+# Toon de naam en het BMI van de personen.
+# Het BMI is het gewicht gedeeld door het kwadraat van de lengte.
+
+# 4.
+# Maak een spel dat de gebruiker een getal tussen 1 en 20 moet raden. Als de gebruiker een cijfer ingeeft
+# dan krijgt hij een hint of het kleiner of groter is. Voeg ook een optie toe zodat de gebruiker kan
+# stoppen indien hij het spel beu is.
+
+# 5.
+# De gebruiker typt een woord. Hij typt daarna een letter.
+# Toon hoeveel keer de letter in het woord voorkomt.
+# Bij het woord programmeur en de letter r toont je: r komt 3 keer voor in programmeur.
+
+# 6.
+# De gebruiker typt een woord.
+# Zeg of dit woord een palindroom is. Een palindroom is een symmetrisch woord
+# dat je zowel van links naar rechts als van rechts naar links kan lezen. Voorbeeld: lepel.
+
+# 7.
+# Maak een lijst met personen. Elke persoon heeft een naam en spaargeld.
+# Toon de namen en het spaargeld van de personen met meer dan € 1000 spaargeld
+
+# 8.
+# De gebruiker typt per land de naam en de oppervlakte. Hij doet dit tot hij stop typt bij de naam.
+# Toon de gemiddelde oppervlakte van de landen.
+# Toon de landen met een oppervlakte onder dit gemiddelde.
+# Toon per land de naam en de oppervlakte.
+# Toon de landen met een oppervlakte boven dit gemiddelde.
+# Toon per land de naam en de oppervlakte.
+
+# 9.
+# De gebruiker typt per persoon de naam en de lengte.
+# Hij herhaalt dit tot hij stop typt bij de naam.
+# Toon de kleinste lengte van de personen.
+# Toon de personen met een lengte gelijk aan de kleinste lengte.
+# Toon per persoon de naam en de lengte.
+# Toon de grootste lengte van de personen.
+# Toon daarna de personen met een lengte gelijk aan de grootste lengte.
+# Toon per persoon de naam en de lengte.
+
+# 10.
+# De gebruiker typt per artikel de naam, de aankoopprijs en de verkoopprijs.
+# Hij herhaalt dit tot hij stop typt bij de naam.
+# Hij tikt daarna een woord.
+# Toon de namen van de artikels die in hun naam dit woord bevatten.
+# De gebruiker tikt twee getallen.
+# Als het tweede getal kleiner is dan het eerste getal toont je een foutmelding.
+# Anders toon je de namen en verkoopprijzen van de artikels waarvan de verkoopprijs ligt tussen
+# deze twee getallen. Als een verkoopprijs gelijk is aan het eerste of het tweede getal neem je het
+# ook op in de lijst.
+# Toon de gemiddelde verkoopprijs van de artikels.
+
+# 11.
+# Maak een lottoformulier met een nested loop en print dit af. Een lotto formulier ziet er zo uit
+# 1 2 3 4 5
+# 6 7 8 9 10
+# 11 ……
+# 41 42 43 44 45
+#  Maak een lottoformulier zonder een nested loop te gebruiken
+
+# 12.
+# Maak een lotto spel. Vraag de gebruiker om 6 getallen en druk dan af als hij de winnende combinatie heeft
+# output
+# --- Lotto ---
+# --- kies uw 6 cijfers ---
+# Geef een getal op (getal 1): 1
+# Geef een getal op (getal 2): 2
+# Geef een getal op (getal 3): 3
+# Geef een getal op (getal 4): 4
+# Geef een getal op (getal 5): 5
+# Geef een getal op (getal 6): 6
+# - De winnende combinatie is : [4, 7, 15, 17, 25, 30]
+# - we gaan nu kijken als u gewonnen heeft
+# U heeft 1 correct cijfers
+# U heeft de volgende cijfers correct: [4]
+
+# 13.
+# Maak een programma dat het volgende afdrukt:
+# Vandaag zijn we: datum
+# Morgen zijn we: datum
+# Gisteren waren we: datum
+# Volgend jaar zijn we het jaar X
+#  Maak een programma dat de geboortedatum van de gebruiker vraagt. Druk het volgende af
+# - zijn geboortedatum inclusief de naam van de dag in het Nederlands
+# voorbeeld: uw geboortedatum is “2/9/1979”, dit was op een zondag
+# - wanneer hij 18 jaar is geworden en de naam van de dag in het Nederlands
+# voorbeeld: u was 18 jaar op datum, dit was op een xxxxx (dag van de week in het Nederlands)
+# - zijn huidige leeftijd
+# voorbeeld: u bent nu x aantal jaar oud)
+# - wanneer hij op pensioen mag gaan (67 jaar, maand nadien)
+# voorbeeld: u mag in “maand” in het “jaar” op pensioen gaan (maand in het Nederlands zetten)
+# Als input gaan we 3 parameters gebruiken: dag maand jaar
+# Als 1 van de parameters ontbreekt gaan we deze alsnog vragen aan de gebruiker
+#  Maak een programma dat elke seconde de tijd afdrukt en dit gedurende 60 seconden
